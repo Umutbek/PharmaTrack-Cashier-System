8546d7dd5cdcb8dd9f0ff200333d83e3faf74e95
Umutbek <karimovumutbek@gmail.com>
2021-02-18 11:58:15 +0600

    Updated

--- app/item/serializers.py
+++ app/item/serializers.py
@@ -94,7 +94,7 @@ class GetItemsInSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.ItemsIn
         fields = (
-            'id', 'storeorderitem', 'address', 'datesent', 'storedepotid', 'depotid', 'iseditable'
+            'id', 'storeorderitem', 'address', 'datesent', 'storedepotid', 'depotid', 'status', 'iseditable'
         )
         read_only_fields = ('id', 'iseditable')
         depth=1
